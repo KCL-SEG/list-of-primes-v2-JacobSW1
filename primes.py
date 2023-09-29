c137bab9491553ab2bc9bfc629bb3f0fcb67ebc5
Jacob Whiting <jacob.whiting@kcl.ac.uk>
2023-09-29 16:37:43 +0100

    Add functionality to primes

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    count = 0
+    number_to_test = 2
+    if number_of_primes <= 0:
+        raise ValueError
+    while count < number_of_primes:
+        for i in range(1, number_to_test) :
+            if number_to_test % i == 0 and number_to_test != i and i != 1:
+                number_to_test +=1
+                continue
+            elif i < number_to_test - 1: 
+                continue 
+            else:
+                count += 1
+                list.append(number_to_test)
+                number_to_test +=1
+                    
     return list
